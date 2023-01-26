3dc124cbcf3f3e7ff958c5082f2294d4c8bb03f9
Daiyan Rafi <daiyanrafi1@gmail.com>
2023-01-26 21:53:33 +0600

    hotels route and model 2152

--- api/index.js
+++ api/index.js
@@ -31,6 +31,9 @@ mongoose.connection.on("connected", () => {
 });
 
 //middlewares
+
+app.use(express.json());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,7 +1,24 @@
 //cookies. jwr web token etc.
 import express from "express";
+import Hotel from "../models/Hotel.js";
 
 const router = express.Router();
 
+//create
+router.post("/", async (req, res) => {
+  //here Hotel is our model, we taking datas from user = new Hotel and creating data
+  const newHotel = new Hotel(req.body);
+
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//update
+//delete
+//get
+//get all
 
 export default router;

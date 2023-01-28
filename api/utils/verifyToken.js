1f223e6565f3aa34fe769751ae53b1bca5fedbf1
Daiyan Rafi <daiyanrafi1@gmail.com>
2023-01-28 13:44:34 +0600

    verify token 281 1344

---
+++ api/utils/verifyToken.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import { createError } from "../utils/error.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return next(createError(401, "authentication faield!"));
+  }
+
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) return next(createError(403, "token not valid!"));
+    //new req property
+    req.user = user;
+    next();
+  });
+};
+//user
+export const verifyUser = (res, req, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "Not authorized!"));
+    }
+  });
+};
+//admin
+export const verifyAdmin = (res, req, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "Not authorized!"));
+    }
+  });
+};

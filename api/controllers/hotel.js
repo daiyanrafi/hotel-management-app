c28716cf822315215511abd007beaf4efd1cdf5c
Daiyan Rafi <daiyanrafi1@gmail.com>
2023-02-01 13:06:41 +0600

    changes on some file about price 012 1306

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -55,7 +55,7 @@ export const getHotels = async (req, res, next) => {
   try {
     const hotels = await Hotel.find({
       ...others,
-      chepestPrice: { $gt: min | 1, $lt: max || 999 },
+      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
     }).limit(req.query.limit);
     console.log({hotels});
     res.status(200).json(hotels);
